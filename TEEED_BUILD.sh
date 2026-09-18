6b343d33ae3c23b12fc66cf289e531d8a1c256c3
Tadeusz Magura-Witkowski <tadeuszmw@gmail.com>
2026-09-18 16:54:54 +0200

    Fixed builder

--- TEEED_BUILD.sh
+++ TEEED_BUILD.sh
@@ -1,1 +1,2 @@
-docker buildx build --platform linux/amd64,linux/arm64 --push -t docker.v.teeed.eu/matterbridge:2 .
+docker buildx create --platform linux/amd64,linux/arm64 --name teeed-builder --buildkitd-config buildkitd.toml
+docker buildx build --builder teeed-builder --platform linux/amd64,linux/arm64 --push -t docker.v.teeed.eu/matterbridge:dev .
